2ed3b670dec090989f4fe0b154c2a91bec6ce139
aik <anthonykrueger0@gmail.com>
2021-07-01 23:46:17 -0500

    added default html route

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const path = require('path')
 
 router.use('/api', apiRoutes);
 
 router.get('/', async (req, res) => {
     try {
-        res.status(200).send("Connected!")
+        res.status(200).sendFile(path.join(__dirname, '../html/index.html'))
     }
     catch {
         res.status(404).send("Oh No!")
     }
-
 })
 
 module.exports = router;
